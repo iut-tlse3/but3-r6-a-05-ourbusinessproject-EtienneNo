fface503fc4060c31ca968e65e0648b83920757d
Etienne NOEL <127329381+EtienneNo@users.noreply.github.com>
2026-01-27 10:51:34 +0100

    replace NotNull with NotBlank + add JavaDoc + use Email instead of Pattern

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,54 +1,80 @@
 package ourbusinessproject;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Enterprise {
-    @NotNull
-    @Size(min = 1)
+    @NotBlank
     private String name;
 
-    @NotNull
-    @Size(min = 10)
+    @NotBlank @Size(min = 10)
     private String description;
 
-    @NotNull
-    @Size(min=1)
+    @NotBlank
     private String contactName;
 
-    @NotNull
-    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    @NotBlank @Email
     private String contactEmail;
 
+    /**
+     * Get the name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get the description
+     * @return
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Set the description
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Get the contact name
+     * @return
+     */
     public String getContactName() {
         return contactName;
     }
 
+    /**
+     * Set the contact name
+     * @param contactName
+     */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
+    /**
+     * Get the contact email
+     * @return
+     */
     public String getContactEmail() {
         return contactEmail;
     }
 
+    /**
+     * Set the contact email
+     * @param contactEmail
+     */
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,26 +1,41 @@
 package ourbusinessproject;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 
 public class Project {
-    @NotNull
-    @Size(min = 1)
+    @NotBlank
     private String title;
+
     private String description;
 
+    /**
+     * Set the title
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Set the description
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Get the title
+     * @return
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Get the description
+     * @return
+     */
     public String getDescription() {
         return description;
     }
