03996b0e7f7b56f72ff7cd3f1c563e589b945bf4
Etienne NOEL <127329381+EtienneNo@users.noreply.github.com>
2026-01-27 11:33:50 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,8 +1,18 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
 
+@Entity
 public class Enterprise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     @NotBlank
     private String name;
 
